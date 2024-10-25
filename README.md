0bb74cdc2b2b2f279d54eef953aafe67791187ff
matheus.janson <matheus.janson@nexxera.com>
2024-10-25 19:28:53 -0300

    backup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # gopportunities
+
+This project/api aims to manage job vacancies. 
+
+Through the endpoints it will be possible:
+
+Register vacancies.
+
+Update vacancies.
+
+Exclude vacancies.
+
+See vacancies.
--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/MatheusJPY/gopportunities/router"
+import (
+	"github.com/MatheusJPY/gopportunities/config"
+	"github.com/MatheusJPY/gopportunities/router"
+)
 
 func main() {
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		panic(err)
+	}
 	// Initialize Router
 	router.Initialize()
 }
